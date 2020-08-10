fb52979721c50d3cf2b9f013dcfd95d695f8fa97
Dipesh Bhagat <dipeshb164@gmail.com>
2020-08-10 11:39:12 +0530

    Updates

---
+++ precache-manifest.cef9a6f5b23176cd6d31795345aff654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf01104e39910355670ad714f8e6c35",
+    "url": "/git-pages/index.html"
+  },
+  {
+    "revision": "6c8b5ede23b795a52fb1",
+    "url": "/git-pages/static/css/main.da133ffc.chunk.css"
+  },
+  {
+    "revision": "94bdad2049fc3929ba6e",
+    "url": "/git-pages/static/js/2.62028b98.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/git-pages/static/js/2.62028b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8b5ede23b795a52fb1",
+    "url": "/git-pages/static/js/main.5615cc0b.chunk.js"
+  },
+  {
+    "revision": "d548593d109ccaef14fe",
+    "url": "/git-pages/static/js/runtime-main.c547d5e1.js"
+  },
+  {
+    "revision": "3a2a92f25316e2c6f9e51cf59f253385",
+    "url": "/git-pages/static/media/Picture2.3a2a92f2.jpg"
+  }
+]);
